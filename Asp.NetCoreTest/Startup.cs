06492d54bfb932073ae37f895ecc0ad1caedc125
yangkuan <yangkuan@DESKTOP-O8R8OPN>
2017-08-24 11:17:30 +0800

    测试提交

--- Asp.NetCoreTest/Startup.cs
+++ Asp.NetCoreTest/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Asp.NetCoreTest.Contexts;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
 
 namespace Asp.NetCoreTest
 {
